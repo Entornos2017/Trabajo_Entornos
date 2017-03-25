415743606126b02d2c86b0e5fb2c5c64e9a02ebe
Entornos2017 <entornos2017@yopmail.com>
2017-03-25 23:41:07 +0100

    Update Ejemplo_1.java

--- src/ejemplo_1/Ejemplo_1.java
+++ src/ejemplo_1/Ejemplo_1.java
@@ -17,8 +17,8 @@ public class Ejemplo_1 {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        System.out.println("Modificado anteiormente");
         
-        System.out.println("Modificado anterior");
         
     }
     
